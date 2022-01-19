7668cc89a619ad1e75762e17a81ff0b6a8929a91
Stan Tapchylka <stanislautapchylka@gmail.com>
2022-01-19 12:31:47 +0100

    Done

--- exercises/01-exe-js_obj_basics.js
+++ exercises/01-exe-js_obj_basics.js
@@ -1,26 +1,30 @@
 const ironhacker = {
-    firstName: 'Tio',
-    age: 39,
-    favoriteLanguage: 'JavaScript',
-    isSatisfied: true,
-    'works at': 'Google',
-    isRemote: false
-}
+  firstName: "Tio",
+  age: 39,
+  favoriteLanguage: "JavaScript",
+  isSatisfied: true,
+  worksAt: "Google",
+  isRemote: false,
+};
 
-// 1: update favoriteLanguage to 'React' 
-// ... your code here
+// 1: update favoriteLanguage to 'React'
+ironhacker.favoriteLanguage = "React";
 
 // 2: Update ironhacker's age to 29
-// ... your code here
+ironhacker.age = 29;
 
 // 3: Remove the 'isRemote' property
-// ... your code here
+delete ironhacker.isRemote;
 
 // 4: Add a new property: 'didGraduate' and set it to a valid boolean value
-// ... your code here
+ironhacker["didGraduate"] = true;
 
 // 5: list all keys of the ironhacker object
-// ... your code here
+for (const key in ironhacker) {
+  console.log(key);
+}
 
 // 6: check if ironhacker has property 'works at'
-// ... your code here
+console.log(ironhacker.hasOwnProperty("worksAt"));
+
+console.log(ironhacker);
